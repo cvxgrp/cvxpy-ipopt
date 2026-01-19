a5939d9496b3bc45956ad1bb3ca7da8a30b8fba5
Daniel <danielcederberg1@gmail.com>
2026-01-19 07:53:52 -0800

    random initial points

--- cvxpy/tests/nlp_tests/stress_tests_diff_engine/test_affine_vector_atoms.py
+++ cvxpy/tests/nlp_tests/stress_tests_diff_engine/test_affine_vector_atoms.py
@@ -8,17 +8,17 @@ from cvxpy.reductions.solvers.nlp_solvers.nlp_solver import DerivativeChecker
 
 @pytest.mark.skipif('IPOPT' not in INSTALLED_SOLVERS, reason='IPOPT is not installed.')
 class TestAffineDiffEngine:
-		# Stress tests for affine atoms in the diff engine.
-		
+    # Stress tests for affine vector atoms in the diff engine.		
     def test_row_broadcast(self):
         # x is 1 x n, Y is m x n
+        np.random.seed(0)
         m, n = 3, 4
         x = cp.Variable((1, n), bounds=[-2, 2])
         Y = cp.Variable((m, n), bounds=[-1, 1])
         obj = cp.Minimize(cp.sum(x + Y))
         prob = cp.Problem(obj)
-        x.value = np.full((1, n), 1.5)  # strictly inside [-2,2]
-        Y.value = np.full((m, n), 0.5)  # strictly inside [-1,1]
+        x.value = np.random.rand(1, n)  
+        Y.value = np.random.rand(m, n)
         checker = DerivativeChecker(prob)
         result = checker.run()
         assert result['objective']
@@ -34,13 +34,14 @@ class TestAffineDiffEngine:
 
     def test_col_broadcast(self):
         # x is m x 1, Y is m x n
+        np.random.seed(0)
         m, n = 3, 4
         x = cp.Variable((m, 1), bounds=[-2, 2])
         Y = cp.Variable((m, n), bounds=[-1, 1])
         obj = cp.Minimize(cp.sum(x + Y))
         prob = cp.Problem(obj)
-        x.value = np.full((m, 1), 1.5)
-        Y.value = np.full((m, n), 0.5)
+        x.value = np.random.rand(m, 1)
+        Y.value = np.random.rand(m, n)
         checker = DerivativeChecker(prob)
         result = checker.run()
         assert result['objective']
@@ -55,6 +56,7 @@ class TestAffineDiffEngine:
         assert np.allclose(Y.value, -1, atol=1e-4)
 
     def test_index_stress(self):
+        np.random.seed(0)
         m, n = 3, 4
         X = cp.Variable((m, n), bounds=[-2, 2])
         expr = (cp.sum(X[0, :]) + cp.sum(X[0, :]) +
@@ -75,6 +77,7 @@ class TestAffineDiffEngine:
         assert np.allclose(prob.value, -34.0)
 
     def test_duplicate_indices(self):
+        np.random.seed(0)
         m, n = 3, 3
         X = cp.Variable((m, n), bounds=[-2, 2])
         # Use duplicate indices: X[[0,0],[1,1]] = [X[0,1], X[0,1]]
@@ -95,13 +98,14 @@ class TestAffineDiffEngine:
 
     def test_promote_row(self):
         # Promote scalar to row vector
+        np.random.seed(0)
         n = 4
         x = cp.Variable(bounds=[-3, 3])
         Y = cp.Variable((1, n), bounds=[-2, 2])
         obj = cp.Minimize(cp.sum(x + Y))
         prob = cp.Problem(obj)
         x.value = 2.0
-        Y.value = np.full((1, n), 1.0)
+        Y.value = np.random.rand(1, n)
         checker = DerivativeChecker(prob)
         result = checker.run()
         assert result['objective']
@@ -117,13 +121,14 @@ class TestAffineDiffEngine:
 
     def test_promote_col(self):
         # Promote scalar to column vector
+        np.random.seed(0)
         m = 4
         x = cp.Variable(bounds=[-3, 3])
         Y = cp.Variable((m, 1), bounds=[-2, 2])
         obj = cp.Minimize(cp.sum(x + Y))
         prob = cp.Problem(obj)
         x.value = 2.0
-        Y.value = np.full((m, 1), 1.0)
+        Y.value = np.random.rand(m, 1)
         checker = DerivativeChecker(prob)
         result = checker.run()
         assert result['objective']
@@ -139,12 +144,13 @@ class TestAffineDiffEngine:
     
     def test_promote_add(self):
         # Scalar x, matrix Y, with bounds set via the bounds attribute
+        np.random.seed(0)
         x = cp.Variable(bounds=[-1, 1])
         Y = cp.Variable((2, 2), bounds=[0, 2])
         obj = cp.Minimize(cp.sum(x + Y))
         prob = cp.Problem(obj)
         x.value = 0.0
-        Y.value = np.full((2, 2), 1.5)
+        Y.value = np.random.rand(2, 2)
         checker = DerivativeChecker(prob)
         result = checker.run()
         assert result['objective']
