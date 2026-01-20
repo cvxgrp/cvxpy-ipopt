a83130346b77f604679e8689d68dbc598ad825c7
Daniel <danielcederberg1@gmail.com>
2026-01-20 10:02:17 -0800

    cleaned up converter of multiply

--- cvxpy/tests/nlp_tests/test_matmul.py
+++ cvxpy/tests/nlp_tests/test_matmul.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pytest
-import scipy.sparse as sp
 
 import cvxpy as cp
 from cvxpy.reductions.solvers.defines import INSTALLED_SOLVERS
@@ -81,48 +80,6 @@ class TestMatmul():
                     derivative_test='none', verbose=True)
         assert(problem.status == cp.OPTIMAL)
         print("successful")
-
-    def test_sparse_matrix(self):
-        n = 10
-        A = np.random.rand(n, n)
-        c = np.random.rand(n, 1)
-        x = cp.Variable((n, 1), nonneg=True)
-        x0 = np.random.rand(n, 1)
-        b = A @ x0
-
-        x.value = 10 * np.ones((n, 1))
-        obj = cp.Minimize(c.T @ x)
-
-        # solve problem with dense A
-        constraints = [A @ x == b]
-        problem = cp.Problem(obj, constraints)
-        problem.solve(solver=cp.IPOPT, nlp=True, verbose=True)
-        dense_val = problem.value
-        dense_sol = x.value
-
-        x.value = 10 * np.ones((n, 1))
-
-        # solve problem with sparse A CSR
-        A_sparse = sp.csr_matrix(A)
-        constraints = [A_sparse @ x == b]
-        problem = cp.Problem(obj, constraints)
-        problem.solve(solver=cp.IPOPT, nlp=True, verbose=True)
-        sparse_val = problem.value
-        sparse_sol = x.value
-
-        x.value = 10 * np.ones((n, 1))
-        # solve problem with sparse A CSC
-        A_sparse = sp.csc_matrix(A)
-        constraints = [A_sparse @ x == b]
-        problem = cp.Problem(obj, constraints)
-        problem.solve(solver=cp.IPOPT, nlp=True, verbose=True)
-        csc_val = problem.value
-        csc_sol = x.value
-
-        assert np.allclose(dense_val, sparse_val)
-        assert np.allclose(dense_val, csc_val)
-        assert np.allclose(dense_sol, sparse_sol)
-        assert np.allclose(dense_sol, csc_sol)
 
     # this test raises an error in derivative oracle
     #@pytest.mark.xfail(reason="derivative oracle fails on this test")
