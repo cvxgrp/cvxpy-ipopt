c6d2ba1f328478f3a58378ed9b0bcef7f86a5c8c
Daniel <danielcederberg1@gmail.com>
2026-01-20 09:31:21 -0800

    added test for sparse matrix vector

--- cvxpy/tests/nlp_tests/test_matmul.py
+++ cvxpy/tests/nlp_tests/test_matmul.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pytest
+import scipy.sparse as sp
 
 import cvxpy as cp
 from cvxpy.reductions.solvers.defines import INSTALLED_SOLVERS
@@ -80,6 +81,44 @@ class TestMatmul():
                     derivative_test='none', verbose=True)
         assert(problem.status == cp.OPTIMAL)
         print("successful")
+
+    def test_sparse_matrix(self):
+        n = 10
+        A = np.random.rand(n, n)
+        c = np.random.rand(n, 1)
+        x = cp.Variable((n, 1), nonneg=True)
+        x0 = np.random.rand(n, 1)
+        b = A @ x0
+
+        obj = cp.Minimize(c.T @ x)
+
+        # solve problem with dense A
+        constraints = [A @ x == b]
+        problem = cp.Problem(obj, constraints)
+        problem.solve(solver=cp.IPOPT, nlp=True)
+        dense_val = problem.value
+        dense_sol = x.value
+
+        # solve problem with sparse A CSR
+        A_sparse = sp.csr_matrix(A)
+        constraints = [A_sparse @ x == b]
+        problem = cp.Problem(obj, constraints)
+        problem.solve(solver=cp.IPOPT, nlp=True)
+        sparse_val = problem.value
+        sparse_sol = x.value
+
+        # solve problem with sparse A CSC
+        A_sparse = sp.csc_matrix(A)
+        constraints = [A_sparse @ x == b]
+        problem = cp.Problem(obj, constraints)
+        problem.solve(solver=cp.IPOPT, nlp=True)
+        csc_val = problem.value
+        csc_sol = x.value
+
+        assert np.allclose(dense_val, sparse_val)
+        assert np.allclose(dense_val, csc_val)
+        assert np.allclose(dense_sol, sparse_sol)
+        assert np.allclose(dense_sol, csc_sol)
 
     # this test raises an error in derivative oracle
     #@pytest.mark.xfail(reason="derivative oracle fails on this test")
